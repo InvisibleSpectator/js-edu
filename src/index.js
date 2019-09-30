b00373e73d37994f426bd7fbfff38590ab504c5f
InvisibleSpectator <vladislav5678@yandex.ru>
2019-09-30 20:22:10 +0400

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+  focus = 'family',
+  knowsProgramming = true,
+  config = { family: 4 }
+) {
+  let hours = 800;
+  if (!knowsProgramming)
+    hours += 500;
+  return Math.ceil(hours / config[focus]);
+};
